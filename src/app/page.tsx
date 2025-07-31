34b0561863d5f8b7d5096d933f8b3afdc06e674d
Ashwin-Pulipati <ashwinpulipati@gmail.com>
2025-07-31 12:35:54 -0400

    init: Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl text-chart-1">Hello World!</h1>
+      <Button variant="secondary">Click Me!</Button>
+    </div>
+  );
+}
