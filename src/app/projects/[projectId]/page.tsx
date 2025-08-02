32343db981d8f27b19559998f7afa8140c5fd509
Ashwin Pulipati <142634979+Ashwin-Pulipati@users.noreply.github.com>
2025-08-02 16:46:59 -0400

    Revert "Feature/chat view"

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,29 +1,12 @@
-import ProjectView from "@/modules/projects/ui/views/project-view";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-
 interface Props {
     params: Promise<{projectId: string}>
 }
 
 const Projects = async ({ params }: Props) => {
-  const { projectId } = await params;
-  
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({
-    projectId,
-  }))
-  void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({
-    id: projectId,
-  }))
+    const { projectId } = await params;
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading project...</div>}>
-        <ProjectView projectId={projectId} />
-      </Suspense>
-    </HydrationBoundary>
-  );
+    <div>Project Id: {projectId}</div>
+  )
 }
 
 export default Projects
--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -3,55 +3,32 @@ import { inngest } from "@/inngest/client";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import z from "zod";
 import { generateSlug } from "random-word-slugs";
-import { TRPCError } from "@trpc/server";
 
 const prisma = new PrismaClient();
 
 export const projectsRouter = createTRPCRouter({
-  getOne: baseProcedure
-    .input(
-      z.object({
-        id: z.string().min(1, { message: "Project ID is required" }),
-      })
-    )
-    .query(async ({ input }) => {
-      const existingProject = await prisma.project.findUnique({
-        where: {
-          id: input.id,
-        }
-      });
-      if (!existingProject) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Project not found",
-        })
-      }
-    return existingProject;
-  }),
-  getMany: baseProcedure.query(async () => {
-    const projects = await prisma.project.findMany({
-      orderBy: {
-        updatedAt: "desc",
-      },
-      // include: {
-      //     fragment: true,
-      // }
-    });
-    return projects;
-  }),
+    getMany: baseProcedure.query(async () => {
+        const projects = await prisma.project.findMany({
+          orderBy: {
+            updatedAt: "desc",
+            },
+            // include: {
+            //     fragment: true,
+            // }
+        })  
+        return projects;
+    }),
   create: baseProcedure
     .input(
       z.object({
-        value: z
-          .string()
-          .min(1, { message: "Value is required" })
-          .max(10000, { message: "Value is too long" }),
+        value: z.string().min(1, { message: "Value is required" })
+        .max(10000, { message: "Value is too long" }),
       })
     )
     .mutation(async ({ input }) => {
       const createdProject = await prisma.project.create({
         data: {
-          name: generateSlug(2, { format: "kebab" }),
+          name: generateSlug(2, {format: "kebab"}),
           messages: {
             create: {
               content: input.value,
@@ -61,14 +38,15 @@ export const projectsRouter = createTRPCRouter({
           },
         },
       });
+      
+        await inngest.send({
+          name: "code-agent/run",
+          data: {
+            value: input.value,
+            projectId: createdProject.id,
+           },
 
-      await inngest.send({
-        name: "code-agent/run",
-        data: {
-          value: input.value,
-          projectId: createdProject.id,
-        },
-      });
-      return createdProject;
+        });
+        return createdProject;
     }),
 });
