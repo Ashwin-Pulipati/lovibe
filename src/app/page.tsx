70bdee9f98ef0c75c63156eb18191e5442cc15d5
Ashwin-Pulipati <ashwinpulipati@gmail.com>
2025-08-01 22:13:23 -0400

    feat(prisma): add agent message flow with Prisma and e2b sandbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 export default function Home() {
   const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(
-    trpc.invoke.mutationOptions({
+  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions())
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
       onSuccess: () => {
         toast.success("Background Job Started");
       },
@@ -20,12 +21,13 @@ export default function Home() {
     <div>
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
-        disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ value: value })}
+        disabled={createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
         variant="secondary"
       >
         Invoke Background Job
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 }
